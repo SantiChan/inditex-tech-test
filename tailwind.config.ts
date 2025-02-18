6385b6f4092c430a27bbc2749facb7b809e031b9
Santi Chan Chao <schan@nakima.es>
2025-02-18 16:27:11 +0000

    fixing tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+const config: Config = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	safelist: ["col-start-1", "col-start-2", "col-start-3"],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			gridTemplateColumns: {
+				subgrid: "repeat(3, minmax(0, 1fr))",
+			},
+		},
+	},
+	plugins: [],
+};
+
+export default config;
